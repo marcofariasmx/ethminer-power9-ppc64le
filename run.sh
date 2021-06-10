48e63b0d512d05652b30029b52bbc5ed37b32b0d
Mx <mx@Mxs-MacBook-Pro.local>
2021-06-09 20:35:20 -0500

    Added run script

---
+++ run.sh
@@ -0,0 +1,8 @@
+export LC_ALL=C; unset LANGUAGE
+
+while true
+do
+  ./ethminer -U -P stratum1+tcp://0x9de66eC1FCa0Fb7D2d3916C1Dd3e9B86bD3B05e6.$HOSTNAME@us1.ethermine.org:4444
+  echo "Program 'ethminer' crashed with exit code.  Respawning.."
+  sleep 1
+done
